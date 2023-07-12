71b075c6d3026cadaad233328c1efe9f3b772cb4
Amy Eben <77935411+amyeben@users.noreply.github.com>
2023-07-12 13:44:35 +0100

    Create test.php

---
+++ the-vintage-house/pages/api/test.php
@@ -0,0 +1,12 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+header("Access-Control-Allow-Methods: *");
+
+
+echo "Hello world ! ";
+
+echo $_POST["email"];
+echo $_POST["password"];
+
